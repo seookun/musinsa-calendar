a0ced49da35f01267eb15527c5b5e19d0c25cb5c
seookun <jseokh@gmail.com>
2022-10-07 03:08:07 +0900

    fix: set `@typescript-eslint/no-unused-vars rule` to error

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,9 +34,10 @@ module.exports = {
   ],
   plugins: ['simple-import-sort'],
   rules: {
-    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
 
     'prettier/prettier': 'error',
     'prefer-const': 'error',
